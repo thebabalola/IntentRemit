9ce5afc15e0aa9d1d645074698d6ed8d950ddbee
thebabalola <t.babalolajoseph@gmail.com>
2026-05-28 04:30:47 +0100

    fix: remove duplicate OnboardingTour mount from ClientLayout

--- frontend/components/ClientLayout.tsx
+++ frontend/components/ClientLayout.tsx
@@ -3,13 +3,11 @@
 import { ReactNode } from "react";
 import Navbar from "./Navbar";
 import { ToastProvider } from "./Toast";
-import OnboardingTour from "./OnboardingTour";
 
 export default function ClientLayout({ children }: { children: ReactNode }) {
   return (
     <ToastProvider>
       <Navbar />
-      <OnboardingTour />
       {children}
     </ToastProvider>
   );
